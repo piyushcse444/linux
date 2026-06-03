c1df6274adaa98099058c32634bdeff5f2d2b712
kali <kali@kali.google.com>
2026-06-03 22:29:14 +0530

    add operators.txt

--- temp/user_kali_script.sh
+++ temp/user_kali_script.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 echo this is the file of kali user
+echo "first argument" $1
+echo "second argument" $2
+echo "total number of arugment" $#
+echo "process id of current shell" $i$
+echo "exit status of last command" $?
+	wjddqk 2> error.txt
+	echo "exit status of last command" $?
